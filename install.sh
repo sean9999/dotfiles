9d35b02c378b00f065671f8eae4fe27d75e3880f
Sean Macdonald <sean@crazyhorsecoding.com>
2015-03-27 21:43:11 -0400

    hello

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo 'downloading...'
 wget -q https://github.com/sean9999/dotfiles/archive/master.zip
-unzip master
+echo 'unpacking...'
+unzip -q master
 cd dotfiles-master/home
+echo 'copying...'
 for f in dot.*
 do
 	echo $f
+	echo ${f//^dot/.}
 done
