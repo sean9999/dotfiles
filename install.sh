0eecfd4b1afaf91d66820f4bceb3e0ef0791da3d
Sean Macdonald <sean@crazyhorsecoding.com>
2015-03-27 21:19:25 -0400

    another typo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl https://github.com/sean9999/dotfiles/archive/master.zip > smac_dotfiles_master.zip
+curl https://github.com/sean9999/dotfiles/archive/master.zip > smac_dotfiles_master
 unzip smac_dotfiles_master
 cd dotfiles-master/home
 for f in dot.*
