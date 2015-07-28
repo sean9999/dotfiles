33f8970b42642509850ee03328552dee016094b5
Sean Macdonald <sean@crazyhorsecoding.com>
2015-07-28 01:26:24 -0400

    improved install script, better readme

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 dotfiles
 ========
 
-these are my dotfiles, and also a way to manage them. You should fork it, and add your own
+these are my dotfiles, and also a way to manage them. You should fork it. It's nice to have your dotfiles wherever you go. Don't you think?
 
 ## requirements
-git, npm, node
+curl, git, npm, node
 
 ## installation
 > curl https://raw.githubusercontent.com/sean9999/dotfiles/master/install.sh | bash
--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-INSTALL_DIR=~/0dotfilesXX
+INSTALL_DIR=~/.dotfiles
 cd ~
 echo "installing into $INSTALL_DIR"
 mkdir -p $INSTALL_DIR
-echo "cloning"
-git clone --recursive https://github.com/sean9999/dotfiles.git $INSTALL_DIR
-git status
+rm $INSTALL_DIR/*
+rm -rvf $INSTALL_DIR/.git 2> /dev/null
+echo "cloning..."
+git clone https://github.com/sean9999/dotfiles.git $INSTALL_DIR
 cd $INSTALL_DIR
+git status
 npm install
-echo 'done.'
+echo >> ~/.bash_profile
+echo '#	dotfiles' >> ~/.bash_profile
+echo "if [ -f $INSTALL_DIR/bootstrap ]; then source $INSTALL_DIR/bootstrap; fi" >> ~/.bash_profile
+source $INSTALL_DIR/bootstrap
+echo 'dotfiles is now installed!'
+dotfiles
