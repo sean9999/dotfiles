093a9ab27dd690cfa18ac66fdf10082f2e4cbc89
Sean Macdonald <sean@crazyhorsecoding.com>
2015-08-09 04:25:02 -0400

    we need a more recent version of node to use Promises

--- install.sh
+++ install.sh
@@ -9,25 +9,20 @@ rm -rvf $INSTALL_DIR/.git 2> /dev/null
 echo "cloning..."
 git clone https://github.com/sean9999/dotfiles.git $INSTALL_DIR
 cd $INSTALL_DIR
-#	node --harmony executable
+
+#	node --harmony
 sudo -Hn mv nodeES6 /usr/local/bin/
 sudo -Hn chmod +x /usr/local/bin/nodeES6
+sudo -Hn npm install --global n
+sudo -Hn n 0.12.7
 
-git status
+#	install CLI
 npm install
-npm link || sudo -Hn npm link
-
-#if [ "$(which node)" == "" ] && [ "$(which nodejs)" != "" ]; then
-#	sudo -Hn ln -s "$(which nodejs)"  `dirname $(which nodejs)`/node
-#	sudo -Hn npm link
-#else
-#	
-#fi
-#
+npm link 2> /dev/null || sudo -Hn npm link
 
 echo >> ~/.bash_profile
 echo '#	dotfiles' >> ~/.bash_profile
 echo "if [ -f $INSTALL_DIR/bootstrap ]; then source $INSTALL_DIR/bootstrap; fi" >> ~/.bash_profile
 source $INSTALL_DIR/bootstrap
 echo 'dotfiles is now installed!'
-dotfiles
+sleep 1 && dotfiles success && dotfiles help credits
