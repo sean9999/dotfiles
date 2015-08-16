6ff0f9c45e522317604e11c4f48bba752aeb1edb
Sean Macdonald <sean@crazyhorsecoding.com>
2015-08-16 11:18:10 -0400

    deleted accidentally duplicated working tree

--- cli/cli.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export DOTFILES_ROOT=$HOME/.dotfiles;
-
-node --harmony $DOTFILES_ROOT/cli/main.js $@;
--- cli/cli/lib/push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $HOME/.dotfiles
-
-git add --all . \
-    && git commit \
-    && git pull \
-    && git push;
