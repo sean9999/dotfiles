fe7c3287a1ffcb7d271cd28872925eb43a831770
Sean Macdonald <sean@crazyhorsecoding.com>
2015-08-24 17:52:18 -0400

    Better handling of editing on Ubuntu EDITOR env variable is not set

--- cli/lib/edit.js
+++ cli/lib/edit.js
@@ -9,7 +9,13 @@ var child_process = require('child_process'),
 
 module.exports 	= function(submodulename,submoduleargs,vars) {
 	return new Promise(function(resolve,reject){
-		var editor	= process.env.EDITOR || "open";
+		var editor = process.env.EDITOR || (function(os){
+			var opener = 'open';
+			if (os == 'linux') {
+				opener = 'xdg-open';
+			}
+			return opener;
+		})(process.platform);
 		var filepath= '~/.dotfiles/available/'.replace('~',process.env.HOME) + submoduleargs[0];
 		var exec	= (editor + ' ' + filepath).split(/\s+/g);
 		var options	= {};
