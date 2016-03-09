7b165d09c8368905cc8ab745c90d055dacf53ad6
Sean Macdonald <sean@crazyhorsecoding.com>
2016-03-09 15:51:34 -0500

    omnibus
    added "git spull" for recursively updating submodules
    touch bash_profile before you append to it, in case it didn't yet exist

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
+#   allows us to run this script from a different dir
 INSTALL_DIR="$HOME/.dotfiles"
-
 cd $INSTALL_DIR
 
 #	install node.js dependencies
@@ -9,6 +9,7 @@ npm install
 npm link 2> /dev/null || sudo -H npm link
 
 #   @TODO: Make this idempotent
+touch ~/.bash_profile
 echo >> ~/.bash_profile
 echo '#	dotfiles' >> ~/.bash_profile
 echo "source $INSTALL_DIR/bootstrap" >> ~/.bash_profile
