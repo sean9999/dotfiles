7599b0e072a2cf135a3e3dae23d680711021989f
Sean Macdonald <sean.r.macdonald@gmail.com>
2022-05-24 14:02:14 -0400

    wip

--- postinstall.sh
+++ postinstall.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-DOTFILES_DIR=~/.eotfiles
-
+DOTFILES_DIR=~/.dotfiles
+RCFILE=~/.bashrc
 mkdir -p $DOTFILES_DIR
+touch $RCFILE
 
+##  add relevant symlinks
 ln -fs $PWD/available $DOTFILES_DIR/available
 ln -fs $PWD/enabled $DOTFILES_DIR/enabled
 ln -fs $PWD/README.md $DOTFILES_DIR/README.md
+ln -fs $PWD/bootstrap $DOTFILES_DIR/bootstrap
+
+#   add source command to .bashrc
+sed -i '/^#.dotfiles/,/^$/d' $RCFILE
+echo '#	dotfiles' >> $RCFILE
+echo 'source ~/.dotfiles/bootstrap' >> $RCFILE
+echo >> $RCFILE
